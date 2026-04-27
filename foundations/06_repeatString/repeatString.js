d2f1ce8b4619678674bf0d0030d07c10ef073d43
Oscar <100163677+oscarandres12@users.noreply.github.com>
2026-04-27 18:15:15 -0400

    Implement function that repeats a string based on a number

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(inputWord, repeatCount) {
+    let repeatedWord = '';
+    if(repeatCount >= 0){
+        for(let count = 0; count < repeatCount; count++){
+            repeatedWord += repeatedWord + inputWord;
+        }
+        return(repeatedWord);
+    }else {
+        return('ERROR');
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
