91b32ff3f381e620c7b18aa6ba3c3ddca7fcbe42
Oscar <100163677+oscarandres12@users.noreply.github.com>
2026-04-27 18:55:37 -0400

    Create function that reverses a string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverseString = '';
+    let wordSize = word.length-1;
+    for( count = 0; count <= wordSize; count ++){
+        reverseString += word[wordSize-count];
+    }
+    return(reverseString);
 };
-
+    
 // Do not edit below this line
 module.exports = reverseString;
