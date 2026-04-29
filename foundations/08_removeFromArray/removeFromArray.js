e54fcb1932345d7e7aa9987511e98cd0818073df
Oscar <100163677+oscarandres12@users.noreply.github.com>
2026-04-29 19:08:34 -0400

    Create function to remove specified elements from an array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let newArray = [];
+    for(let count = 1 ; count < arguments.length; count++){
+        for(let count2 = 0 ; count2 < array.length ; count2++){
+            if(array[count2] !== arguments[count]){
+                newArray.push(array[count2])
+            }
+        }
+        array = [...newArray];
+        newArray = [];
+    }
+    return(array);
 };
 
 // Do not edit below this line
