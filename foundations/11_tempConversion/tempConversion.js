85f51d65a52f5dd4f4bff83f570a9051dc746227
Oscar <100163677+oscarandres12@users.noreply.github.com>
2026-04-30 11:26:08 -0400

    Implement functions to convert Celsius to Fahrenheit and vice versa

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesFahrenheit) {
+  let degreesCelsius = (degreesFahrenheit - 32) * (5/9);
+  let roundedCelsius = Math.round(degreesCelsius * 10) / 10;
+  return(roundedCelsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesCelsius) {
+  let degreesFahrenheit = degreesCelsius * (9 / 5) + 32;
+  let roundedFahrenheit = Math.round(degreesFahrenheit * 10) / 10;
+  return(roundedFahrenheit);
 };
 
 // Do not edit below this line
