4a8194d85f7a10f16b51bfc9547d73af5b29cbcf
Oscar <100163677+oscarandres12@users.noreply.github.com>
2026-04-30 11:59:27 -0400

    Implement a function that substract two numbers

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -2,8 +2,8 @@ const add = function(number1, number2) {
 	return(number1 + number2);
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return(number1 - number2);
 };
 
 const sum = function() {
