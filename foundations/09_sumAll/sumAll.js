659bd99482a8759298bceb81ea1e16f3432f4355
Oscar <100163677+oscarandres12@users.noreply.github.com>
2026-04-29 20:42:28 -0400

    Add function to sum all integers between two given numbers (inclusive)

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(inputNumber1, inputNumber2) {
+    
+    if(inputNumber1 < 0 || inputNumber2 < 0) {
+        return('ERROR')
+    }else if (Number.isInteger(inputNumber1) && Number.isInteger(inputNumber2)){
+        if(inputNumber1 > inputNumber2) {
+            let aux = inputNumber1;
+            inputNumber1 = inputNumber2;
+            inputNumber2 = aux;
+        }
+    }else {
+        return('ERROR')
+    }
 
+    let sum = 0;
+    while(inputNumber1 <= inputNumber2){
+        sum += inputNumber1;
+        ++inputNumber1;
+    }
+    return(sum);
 };
 
 // Do not edit below this line
