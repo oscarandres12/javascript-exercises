1b67a12811abd1fdcd5ab807b124ed1813a8fe07
Oscar <100163677+oscarandres12@users.noreply.github.com>
2026-04-30 20:49:53 -0400

    Implement function to compute the nth Fibonacci number

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibonacciNumber) {
+    if(fibonacciNumber >= 0){
+        let aux1 = 1;
+        let aux2 = -1;
+        let sum = 0;
+        for(let count = 0 ; count <= fibonacciNumber ; count++){
+            sum = aux1 + aux2;
+            aux2 = aux1;
+            aux1 = sum;
+        }
+        return(sum);
+    }else{
+        return('OOPS');
+    }
 };
 
 // Do not edit below this line
