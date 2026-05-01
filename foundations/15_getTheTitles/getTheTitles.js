4ad61c31424fd515497a26a992fc885197357b1c
Oscar <100163677+oscarandres12@users.noreply.github.com>
2026-04-30 21:00:32 -0400

    Add function to return array of book titles

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = [];
+    for(let count = 0; count < books.length; count++){
+        titles.push(books[count].title);
+    }
+    return(titles);
 };
 
 // Do not edit below this line
