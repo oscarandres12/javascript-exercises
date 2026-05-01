4601f651bb950f7f7bcfe328fb8072f3032b93a4
Oscar <100163677+oscarandres12@users.noreply.github.com>
2026-04-30 20:18:52 -0400

    Implement a function to determine if a string is a palindrome

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = string.replace(/\W/g, "").toLowerCase();
+    let reverseString = newString.split("").reverse().join("");
+    // let c = b.reverse();
+    // let d = c.join("");
+    if(newString == reverseString){
+        return(true);
+    }else{
+        return(false);
+    }
 };
 
 // Do not edit below this line
