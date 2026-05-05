2e21b72df195e5a3c425cb10b9d438fbaef373ec
Oscar <100163677+oscarandres12@users.noreply.github.com>
2026-05-05 12:33:54 -0400

    Implement a function to find the oldest person

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    return people.map((person) => {
+        if('yearOfDeath' in person === false){
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        return person
+    }).reduce((accumulator, currentValue) => {
+        if(currentValue.yearOfDeath - currentValue.yearOfBirth > accumulator.yearOfDeath - accumulator.yearOfBirth){
+            return currentValue;
+        }else{
+            return accumulator;
+        }
+    })
+}      
 
 // Do not edit below this line
 module.exports = findTheOldest;
